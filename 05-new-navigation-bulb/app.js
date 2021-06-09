7370b3a6e7ef4ed9f5e16c8b016be430eb96756d
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-09 18:24:45 +0430

    add js plan for focuse

---
+++ 05-new-navigation-bulb/app.js
@@ -0,0 +1,30 @@
+// get Elemnt
+    // bgbody
+    // body
+    // menu
+    // buttons:array
+    // bulb
+    // activeItem
+
+
+
+
+
+// click(item,index)
+    // if (item == active)return
+    // if(active)return remove active
+    // item add active
+    // body bg = [index]
+    // active = item
+    // offsetbulb(active)
+
+//offsetbulb(active)
+     // active.cordineate
+     // const left 
+     // bulb style 3dtransform
+     
+// foreach --> buttons
+    // add eventlistener 
+        // click(item,index)
+    
+
