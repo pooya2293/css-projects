002e0ac971e9f9c1e2f58bee1f6be4e41435cccd
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-06-09 18:26:02 +0430

    get some element on body

--- 05-new-navigation-bulb/app.js
+++ 05-new-navigation-bulb/app.js
@@ -1,11 +1,16 @@
 // get Elemnt
     // bgbody
+    const bgBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
     // body
+    const body = document.body;
     // menu
+    const menu = body.querySelector('.menu');
     // buttons:array
+    const buttons = body.querySelectorAll('.menu__item');
     // bulb
+    const bulb = body.querySelector('.menu__border');
     // activeItem
-
+    let activeItem = body.querySelector('.active');
 
 
 
